4d42361bacbd9a845586614b89422a50138f3733
NickEsColR <nicoesol0818@gmail.com>
2025-10-03 16:38:06 -0500

    Agregar función chunks_pdf para cargar y dividir documentos PDF en fragmentos

---
+++ app/src/utils/text_preprocessor.py
@@ -0,0 +1,18 @@
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_core.documents import Document
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+
+def chunks_pdf(directory_path: str) -> list[Document]:
+    document_loader = PyPDFDirectoryLoader(directory_path)
+    documents = document_loader.load()
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=2000,
+        chunk_overlap=200,
+        length_function=len,
+        add_start_index=True,
+    )
+    chunks = text_splitter.split_documents(documents)
+
+    return chunks
